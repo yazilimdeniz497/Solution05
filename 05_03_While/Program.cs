d0e964af7d4e9f3dab9999534a7eadf82a30abed
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-05-19 14:46:31 +0300

    while döngüsü eklendi

---
+++ 05_03_While/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] numbers = { 0, 20, 30, 70, 5, -20, 50 };
+int i = 0;
+while (i < numbers.Length)
+{
+    if(numbers[i] < 0)
+    {
+        i++;
+        continue;
+      
+       
+
+    }
+    Console.WriteLine($"Numaralar:{numbers[i]}");
+    i++;
+
+
+
+}
+Console.ReadKey();
