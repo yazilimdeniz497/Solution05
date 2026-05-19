df80147509efa4bb2ddd2c7fa5552e99c1d5a58f
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-05-19 14:24:16 +0300

    foreach projesi eklendi

---
+++ 05_02_Foreach/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] numbers = { 1, 7, 5, 6 };
+int sum = 0;
+foreach(var num in numbers)
+{
+    sum += num;
+
+}
+double average = (double)sum / numbers.Length;
+Console.WriteLine($"Ortalamanız:{average}");
+Console.ReadKey();
