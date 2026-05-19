92b26bfa628681e32c0d363ccc8826791310908a
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-05-19 16:51:14 +0300

    solution bitti final

---
+++ 05_05_StudentApp/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _05_05_StudentApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] grades = { 50, 70, 80, 90, 85 };
+            double average = calculateAverage(grades);
+            Console.WriteLine(average);
+            Console.ReadKey();
+        }
+
+        private static double calculateAverage(int[] grades)
+        {
+            int sum=0;
+            foreach(var grade in grades)
+            {
+                sum += grade;
+            
+
+
+            }
+            double average = sum / grades.Length;
+            return average;
+
+        }
+    }
+}
