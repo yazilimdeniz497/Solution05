e0ff404c43d49e247232b6700e966ff368495e31
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-05-19 14:08:04 +0300

    final bu projee bitti

--- 05_01_Arrays/Program.cs
+++ 05_01_Arrays/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//dizi tanımlama
+int[] numbers= new int[5];
+//değer atama
+numbers[0] = 10;
+numbers[1] = 20;
+numbers[2] = 30;
+numbers[3] = 40;
+numbers[4] = 50;
+Console.WriteLine("Dizi elemanlarını tanımlama");
+for(int i=0; i<numbers.Length; i++)
+{
+    Console.WriteLine(numbers[i]);
+}
+for (int i = numbers.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(numbers[i]);
+}
+Console.ReadKey();
+
