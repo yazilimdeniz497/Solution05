ab0d720a500232a54ccff24eb436b4ec7c4ae088
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-05-19 16:14:26 +0300

    Do while eklendi

---
+++ 05_04_DoWhile/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] grades=new int[3];
+int i = 0;
+do
+{
+
+    Console.WriteLine("Not Giriniz: ");
+    grades[i] = Convert.ToInt32(Console.ReadLine());
+    i++;
+} while (i < grades.Length);
+foreach(var grade in grades)
+{
+    Console.WriteLine($"Notlar:{grade}");
+}
+Console.ReadKey();
